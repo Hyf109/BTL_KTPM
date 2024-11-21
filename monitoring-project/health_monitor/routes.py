f4a34886bc7402b21e619751aec4e0f35fde6cf6
nhneptune <124415548+nhneptune@users.noreply.github.com>
2024-11-21 22:33:44 +0700

    fix module not found

--- monitoring-project/health_monitor/routes.py
+++ monitoring-project/health_monitor/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
-from health_monitor.services.external_api import check_external_api
-from health_monitor.services.system import check_system_health
+from services.external_api import ExternalAPI
+from services.system_api import SystemAPI
 import time
 
 routes = Blueprint('routes', __name__)
@@ -9,10 +9,10 @@ start_time = time.time()  # Lưu thời gian bắt đầu chạy ứng dụng
 @routes.route('/health', methods=['GET'])
 def health_check():
     uptime = time.time() - start_time
-    exchange_api_status = check_external_api("http://localhost:8001/exchange-rates")
-    gold_api_status = check_external_api("http://localhost:8002/gold-prices")
+    exchange_api_status = ExternalAPI.check_external_api("http://localhost:8001/exchange-rates")
+    gold_api_status = ExternalAPI.check_external_api("http://localhost:8002/gold-prices")
     
-    system_health = check_system_health()
+    system_health = SystemAPI.check_system_health()
 
     is_healthy = gold_api_status and exchange_api_status and system_health["status"] == "healthy"
     return jsonify({
---
+++ monitoring-project/health_monitor/services/system_api.py
@@ -0,0 +1,16 @@
+import psutil
+
+class SystemAPI:
+    @staticmethod
+    def check_system_health():
+        try:
+            cpu_usage = psutil.cpu_percent(interval=1)
+            memory_usage = psutil.virtual_memory().percent
+
+            return {
+                "status": "healthy" if cpu_usage < 80 and memory_usage < 80 else "unhealthy",
+                "cpu_usage": cpu_usage,
+                "memory_usage": memory_usage
+            }
+        except Exception as e:
+            return {"status": "unhealthy", "cpu_usage": None, "memory_usage": None}
